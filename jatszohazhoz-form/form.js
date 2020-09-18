634f2d6db1ed53791e1efe37e54efb87ec29c91e
sabalazs <sabalazs@gmail.com>
2020-09-18 12:18:42 +0200

    added unselect function

--- jatszohazhoz-form/form.js
+++ jatszohazhoz-form/form.js
@@ -1,3 +1,4 @@
+//filter
 $(document).ready(function () {
     $("#myInput").on("keyup", function () {
         var value = $(this).val().toLowerCase();
@@ -6,3 +7,32 @@ $(document).ready(function () {
         });
     });
 });
+
+//select a game
+$(document).ready(function () {
+    $(".selectbtn").click(function () {
+        
+        let card = $(this).parent().parent();
+        let game = ($(this).siblings(".card-title").text());
+
+        $("#unwanted-games").append("<li class='list-group-item'>" + game + "</li>");
+
+        //remove card from the list
+        card.addClass("selected");
+        card.removeClass("card");
+        card.toggle();
+
+        //add unselect function
+        $("#unwanted-games :last-child").click(function () {
+            for (let i = 0; i < $(".selected").length; i++) {
+                if ($($(".selected")[i]).find(".card-title").text() == $(this).text()) {
+                    $($(".selected")[i]).addClass("card");
+                    $($(".selected")[i]).toggle();
+                    $($(".selected")[i]).removeClass("selected");
+                    $(this).remove();                    
+                }
+            }
+        });
+    });
+});
+
